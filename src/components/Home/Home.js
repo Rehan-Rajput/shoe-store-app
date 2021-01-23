1e959f1b08d9bb5dc1e52190b6e5c39c635044c5
Rehan-Rajput <77195218+Rehan-Rajput@users.noreply.github.com>
2021-01-23 14:15:49 -0500

    updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div className={styles.home}>
       <div className={styles.nav}>
-      <h1>Shoes_Store</h1>
+      <h1>Shoes-Store</h1>
         <nav>
           <ul>
             <li><Link to="/">Home</Link></li>
